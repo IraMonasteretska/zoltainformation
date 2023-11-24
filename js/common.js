be8b37294e8c93f0fc73dd5701a5d1a578297eb9
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-11-24 17:12:29 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
-    $('.query-switch a').click(function(e){
+    // select2
+    if ($('select').length) {
+        $('.styledselect').select2({
+            minimumResultsForSearch: -1,
+            dropdownCssClass: "headerselectdropdown"
+        });
+    }
+
+    $('.query-switch a').click(function (e) {
         e.preventDefault();
         $(this).parents('.query-switch').find('a').removeClass('active');
         $(this).addClass('active');
     });
 
-    $('.mobnemu').click(function(){
+    // open menu (mob)
+    $('.mobnemu').click(function () {
         var overlay = $('<div class="overlay"></div>');
         $('body').append(overlay);
         overlay.fadeIn();
@@ -14,10 +23,331 @@ $(document).ready(function () {
         $('.querysidebar').addClass('show');
     });
 
-    $(document).on('click', '.overlay', function() {
-        $('.overlay').fadeOut(); 
+    $(document).on('click', '.overlay', function () {
+        $('.overlay').fadeOut();
         $('.querysidebar').removeClass('show');
         $('.overlay').remove();
     });
+
+    // text editor
+    if ($('#editor').length) {
+        CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
+            toolbar: {
+                items: [
+                    // 'exportPDF','exportWord', '|',
+                    // 'findAndReplace', 
+                    // 'selectAll', '|',
+                    'heading', '|',
+                    'bold', 'italic', 'strikethrough', 'underline',
+                    // 'code', 'subscript', 'superscript', 'removeFormat', '|',
+                    'bulletedList', 'numberedList',
+                    // 'todoList', '|',
+                    // 'outdent', 'indent', '|',
+                    // 'undo', 'redo',
+                    // '-',
+                    // 'fontSize', 'fontFamily', 'fontColor', 
+                    // 'fontBackgroundColor', 
+                    // 'highlight', '|',
+                    'alignment', '|',
+                    'link', 'insertImage', 'blockQuote',
+                    // 'insertTable',
+                    'mediaEmbed',
+                    //   'codeBlock', 
+                    // 'htmlEmbed', '|',
+                    // 'specialCharacters', 'horizontalLine', 'pageBreak', '|',
+                    // 'textPartLanguage', '|',
+                    // 'sourceEditing'
+                ],
+                shouldNotGroupWhenFull: true
+            },
+            // Changing the language of the interface requires loading the language file using the <script> tag.
+            // language: 'es',
+            list: {
+                properties: {
+                    styles: true,
+                    startIndex: true,
+                    reversed: true
+                }
+            },
+            heading: {
+                options: [
+                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                    { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+                ]
+            },
+            placeholder: 'Add a text description...',
+            fontFamily: {
+                options: [
+                    'default',
+                    'Arial, Helvetica, sans-serif',
+                    'Courier New, Courier, monospace',
+                    'Georgia, serif',
+                    'Lucida Sans Unicode, Lucida Grande, sans-serif',
+                    'Tahoma, Geneva, sans-serif',
+                    'Times New Roman, Times, serif',
+                    'Trebuchet MS, Helvetica, sans-serif',
+                    'Verdana, Geneva, sans-serif'
+                ],
+                supportAllValues: true
+            },
+            fontSize: {
+                options: [10, 12, 14, 'default', 18, 20, 22],
+                supportAllValues: true
+            },
+            htmlSupport: {
+                allow: [
+                    {
+                        name: /.*/,
+                        attributes: true,
+                        classes: true,
+                        styles: true
+                    }
+                ]
+            },
+            htmlEmbed: {
+                showPreviews: true
+            },
+            link: {
+                decorators: {
+                    addTargetToExternalLinks: true,
+                    defaultProtocol: 'https://',
+                    toggleDownloadable: {
+                        mode: 'manual',
+                        label: 'Downloadable',
+                        attributes: {
+                            download: 'file'
+                        }
+                    }
+                }
+            },
+            mention: {
+                feeds: [
+                    {
+                        marker: '@',
+                        feed: [
+                            '@apple', '@bears', '@brownie', '@cake', '@cake', '@candy', '@canes', '@chocolate', '@cookie', '@cotton', '@cream',
+                            '@cupcake', '@danish', '@donut', '@dragée', '@fruitcake', '@gingerbread', '@gummi', '@ice', '@jelly-o',
+                            '@liquorice', '@macaroon', '@marzipan', '@oat', '@pie', '@plum', '@pudding', '@sesame', '@snaps', '@soufflé',
+                            '@sugar', '@sweet', '@topping', '@wafer'
+                        ],
+                        minimumCharacters: 1
+                    }
+                ]
+            },
+            removePlugins: [
+                // 'ExportPdf',
+                // 'ExportWord',
+                'CKBox',
+                'CKFinder',
+                'EasyImage',
+                // This sample uses the Base64UploadAdapter to handle image uploads as it requires no configuration.
+                // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/base64-upload-adapter.html
+                // Storing images as Base64 is usually a very bad idea.
+                // Replace it on production website with other solutions:
+                // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/image-upload.html
+                // 'Base64UploadAdapter',
+                'RealTimeCollaborativeComments',
+                'RealTimeCollaborativeTrackChanges',
+                'RealTimeCollaborativeRevisionHistory',
+                'PresenceList',
+                'Comments',
+                'TrackChanges',
+                'TrackChangesData',
+                'RevisionHistory',
+                'Pagination',
+                'WProofreader',
+                // Careful, with the Mathtype plugin CKEditor will not load when loading this sample
+                // from a local file system (file://) - load this site via HTTP server if you enable MathType
+                'MathType'
+            ]
+        });
+
+    }
+
+    // Upload files
+    var $fileInput = $('.file-input');
+    var $droparea = $('.file-drop-area');
+
+    $fileInput.on('dragenter focus click', function () {
+        $droparea.addClass('is-active');
+    });
+
+    $fileInput.on('dragleave blur drop', function () {
+        $droparea.removeClass('is-active');
+    });
+
+    $fileInput.on('change', function () {
+        var filesCount = $(this)[0].files.length;
+        var $textContainer = $(this).prev();
+
+        if (filesCount === 1) {
+            // if single file is selected, show file name
+            var fileName = $(this).val().split('\\').pop();
+            $textContainer.text(fileName);
+        } else {
+            // otherwise show number of files
+            $textContainer.text(filesCount + ' files selected');
+        }
+    });
+
+
+    // change status - btns position 
+
+    if ($('.changestatus').length) {
+        var top = $('.changestatus').position().top;
+        $('.changestatusbtns').css('top', top + 30);
+    }
+    // cancel change status
+    $('.changestatusbtns .cancel').click(function () {
+        $('.changestatusbtns').hide();
+    })
+
+    if ($('#editor2').length) {
+        CKEDITOR.ClassicEditor.create(document.getElementById("editor2"), {
+            toolbar: {
+                items: [
+                    // 'exportPDF','exportWord', '|',
+                    // 'findAndReplace', 
+                    // 'selectAll', '|',
+                    'heading', '|',
+                    'bold', 'italic', 'strikethrough', 'underline',
+                    // 'code', 'subscript', 'superscript', 'removeFormat', '|',
+                    'bulletedList', 'numberedList',
+                    // 'todoList', '|',
+                    // 'outdent', 'indent', '|',
+                    // 'undo', 'redo',
+                    // '-',
+                    // 'fontSize', 'fontFamily', 'fontColor', 
+                    // 'fontBackgroundColor', 
+                    // 'highlight', '|',
+                    'alignment', '|',
+                    'link', 'insertImage', 'blockQuote',
+                    // 'insertTable',
+                    'mediaEmbed',
+                    //   'codeBlock', 
+                    // 'htmlEmbed', '|',
+                    // 'specialCharacters', 'horizontalLine', 'pageBreak', '|',
+                    // 'textPartLanguage', '|',
+                    // 'sourceEditing'
+                ],
+                shouldNotGroupWhenFull: true
+            },
+            // Changing the language of the interface requires loading the language file using the <script> tag.
+            // language: 'es',
+            list: {
+                properties: {
+                    styles: true,
+                    startIndex: true,
+                    reversed: true
+                }
+            },
+            heading: {
+                options: [
+                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                    { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+                ]
+            },
+            placeholder: 'Add a text description...',
+            fontFamily: {
+                options: [
+                    'default',
+                    'Arial, Helvetica, sans-serif',
+                    'Courier New, Courier, monospace',
+                    'Georgia, serif',
+                    'Lucida Sans Unicode, Lucida Grande, sans-serif',
+                    'Tahoma, Geneva, sans-serif',
+                    'Times New Roman, Times, serif',
+                    'Trebuchet MS, Helvetica, sans-serif',
+                    'Verdana, Geneva, sans-serif'
+                ],
+                supportAllValues: true
+            },
+            fontSize: {
+                options: [10, 12, 14, 'default', 18, 20, 22],
+                supportAllValues: true
+            },
+            htmlSupport: {
+                allow: [
+                    {
+                        name: /.*/,
+                        attributes: true,
+                        classes: true,
+                        styles: true
+                    }
+                ]
+            },
+            htmlEmbed: {
+                showPreviews: true
+            },
+            link: {
+                decorators: {
+                    addTargetToExternalLinks: true,
+                    defaultProtocol: 'https://',
+                    toggleDownloadable: {
+                        mode: 'manual',
+                        label: 'Downloadable',
+                        attributes: {
+                            download: 'file'
+                        }
+                    }
+                }
+            },
+            mention: {
+                feeds: [
+                    {
+                        marker: '@',
+                        feed: [
+                            '@apple', '@bears', '@brownie', '@cake', '@cake', '@candy', '@canes', '@chocolate', '@cookie', '@cotton', '@cream',
+                            '@cupcake', '@danish', '@donut', '@dragée', '@fruitcake', '@gingerbread', '@gummi', '@ice', '@jelly-o',
+                            '@liquorice', '@macaroon', '@marzipan', '@oat', '@pie', '@plum', '@pudding', '@sesame', '@snaps', '@soufflé',
+                            '@sugar', '@sweet', '@topping', '@wafer'
+                        ],
+                        minimumCharacters: 1
+                    }
+                ]
+            },
+            removePlugins: [
+                // 'ExportPdf',
+                // 'ExportWord',
+                'CKBox',
+                'CKFinder',
+                'EasyImage',
+                // This sample uses the Base64UploadAdapter to handle image uploads as it requires no configuration.
+                // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/base64-upload-adapter.html
+                // Storing images as Base64 is usually a very bad idea.
+                // Replace it on production website with other solutions:
+                // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/image-upload.html
+                // 'Base64UploadAdapter',
+                'RealTimeCollaborativeComments',
+                'RealTimeCollaborativeTrackChanges',
+                'RealTimeCollaborativeRevisionHistory',
+                'PresenceList',
+                'Comments',
+                'TrackChanges',
+                'TrackChangesData',
+                'RevisionHistory',
+                'Pagination',
+                'WProofreader',
+                // Careful, with the Mathtype plugin CKEditor will not load when loading this sample
+                // from a local file system (file://) - load this site via HTTP server if you enable MathType
+                'MathType'
+            ]
+        });
+
+    }
+
+    // tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 });
