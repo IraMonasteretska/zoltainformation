a26aa82c70d385ab39d841c6e78b7e3ea73d3808
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-11-28 18:09:04 +0200

    Add files via upload

--- js/sign-js.js
+++ js/sign-js.js
@@ -9,6 +9,31 @@ $(document).ready(function () {
         }
     });
 
+     // verification code
+     const inputsNL = document.querySelectorAll('.number_input');
+
+     const inputsList = Array.prototype.slice.call(inputsNL);
+ 
+     inputsList.forEach((input, index) => {
+         input.addEventListener('keyup', function (ev) {
+             if (ev.which === 69) return input.value = '';
+             let value = input.value;
+             let len = value.length;
+             if (ev.which === 8 && inputsList[index - 1]) {
+                 return inputsList[index - 1].focus();
+             }
+             if (len === 1) {
+                 input.value = value.substr(0, 1);
+                 if (inputsList[index + 1]) inputsList[index + 1].focus();
+             } else if (inputsList[index + 1] && ev.which != 8) {
+                 input.value = value.substr(0, 1);
+                 inputsList[index + 1].focus();
+                 inputsList[index + 1].value = value.substr(1, 1);
+             } else if (len > 1 && !inputsList[index + 1]) {
+                 input.value = value.substr(0, 1);
+             }
+         })
+     })
 
 
 });
